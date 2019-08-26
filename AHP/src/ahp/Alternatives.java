76ab631666f3d05ae9c9bd8dc4c6893d66dbbb4f
Natan Santana de Morais <natasantanademorais@gmail.com>
2019-08-25 22:02:36 -0300

    Change of Alternatives.java and include of Observação.txt

--- AHP/src/ahp/Alternatives.java
+++ AHP/src/ahp/Alternatives.java
@@ -30,16 +30,26 @@ public class Alternatives {
     }
     
     public void buildAndCompute(String[] labelsAlternatives, double[][] compArray) {
-    	int i;
+    	int i,j;
+    	
     	for (i = 0; i < this.nrCriterion; i++) {
     		this.tableCriterion[i] = new TableCriterion();
     		System.out.println("------------------------------------------------");
     		this.tableCriterion[i].Compute(this.nrAlternatives, labelsAlternatives, compArray[i]);
     	}
     	
-    	i = 0;
-    	for (i = 0; i < this.nrCriterion; i++) {
-    		
+    	double[] priorityWeightsFinal = new double[this.nrAlternatives];
+    	
+    	System.out.println("\nFinal priority");
+    	
+    	for (i = 0; i < this.nrAlternatives; i++) {
+    		double soma = 0;
+    		for (j = 0; j < this.nrCriterion; j++) {
+    			soma = soma + (this.tableCriterion[j].getWeights()[i] * this.weightsFinal[j]);
+    		}
+    		priorityWeightsFinal[i] = soma;
+
+    		System.out.println(labelsAlternatives[i] + " : " + priorityWeightsFinal[i] * 100);
     	}
     }    
 }
